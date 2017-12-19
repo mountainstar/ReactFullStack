808eb9620911cdac931eb453e7129a68979fffce
Chris Wirth <chris@goozmo.com>
2017-12-19 12:36:34 -0700

    proxy fix

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecert,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
