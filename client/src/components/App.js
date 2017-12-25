6935c95e9cce0f7423f41411a9342278694559f1
Chris Wirth <chris@goozmo.com>
2017-12-25 12:38:00 -0700

    redux setup

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hi There woo!
+    </div>
+  )
+}
+
+export default App;
---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js don't commit this!!!
+
+module.exports = {
+  googleClientID: '692361642644-aapqar68jkecak68ccn2tr1tbmhppobq.apps.googleusercontent.com',
+  googleClientSecert: '6WfdxsfWB06sM1vHhZepLvmO',
+  mongoUri: 'mongodb://chriswirth:Ph53n4x420@ds145395.mlab.com:45395/emaily-dev',
+  cookieKey: 'qonidqohidwb9vb9dfw894732hnjdi2894qhr'
+};
