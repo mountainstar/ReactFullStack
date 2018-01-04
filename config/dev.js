07142508c8789ce37612b56ff1df07163ff71ebb
Chris Wirth <chris@goozmo.com>
2018-01-03 22:26:38 -0700

    Added billing and client side app

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientID: '692361642644-aapqar68jkecak68ccn2tr1tbmhppobq.apps.googleusercontent.com',
   googleClientSecert: '6WfdxsfWB06sM1vHhZepLvmO',
   mongoUri: 'mongodb://chriswirth:Ph53n4x420@ds145395.mlab.com:45395/emaily-dev',
-  cookieKey: 'qonidqohidwb9vb9dfw894732hnjdi2894qhr'
+  cookieKey: 'qonidqohidwb9vb9dfw894732hnjdi2894qhr',
+  stripePublishableKey: 'pk_test_z6kLtSPXLmHU7AmKogywGANo',
+  stripeSecretKey: 'sk_test_jlQJ71ya64YKAgpYYBcSSspO'
 };
